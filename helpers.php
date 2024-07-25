46d20fd192e8f3bd2c4d22abfb17842fb4fd7fa2
Mary Anne Fusco <fuscomaryanne@gmail.com>
2024-07-24 23:00:30 -0400

    simple routes

--- helpers.php
+++ helpers.php
@@ -43,3 +43,29 @@
       echo "Partial '{$name} not found! '";
     }
   }
+
+  /**
+   * Inspect a value(s)
+   * 
+   * @param mixed $value
+   * @return void
+   */
+
+   function inspect($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+   }
+   
+/**
+   * Inspect a value(s) and die
+   * 
+   * @param mixed $value
+   * @return void
+   */
+
+   function inspectAndDie($value){
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+   }
